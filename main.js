61f0bbca5c5bf1191ee9c0b12d4740e008c45f21
exchavez09 <76718764+exchavez09@users.noreply.github.com>
2021-06-23 20:22:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(450,400);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('Img1.png');
+}
